f8b32f4fbb08a03ac12545bb32d26b5c502c3c63
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-21 15:52:29 +0330

    add methods to Advertiser class

--- src/adModels/Advertiser.java
+++ src/adModels/Advertiser.java
@@ -13,4 +13,30 @@ public class Advertiser extends BaseAdvertising {
         this.name = name;
         Advertiser.allAdvertisers.add(this);
     }
+
+    public static int getTotalClicks() {
+        return Advertiser.allAdvertisers.stream().mapToInt(BaseAdvertising::getClicks).sum();
+    }
+
+    public static void help() {
+        System.out.println("Advertiser Fields ->\n" +
+                "id: unique id of advertiser\n" +
+                "name: name of this advertiser\n" +
+                "views: number of total views of this advertiser's ads\n" +
+                "clicks: number of total clicks of this advertiser's ads");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void describeMe() {
+        System.out.println("Name: Advertiser\n" +
+                "Description: This class is a blueprint to create advertiser objects and derived from BaseAdvertising Base class.");
+    }
 }
