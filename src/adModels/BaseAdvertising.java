3a12d4e9edcb7e43aaad917f580515cb46f98bd2
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-21 13:24:51 +0330

    Add methods to BaseAdvertising and change class to non abstract

--- src/adModels/BaseAdvertising.java
+++ src/adModels/BaseAdvertising.java
@@ -1,13 +1,33 @@
 package adModels;
 
-public abstract class BaseAdvertising {
+public class BaseAdvertising {
     private int id;
     private int clicks;
     private int views;
 
-    public BaseAdvertising(int id) {
-        this.id = id;
+    public BaseAdvertising() {
         this.clicks = 0;
         this.views = 0;
+    }
+
+    public int getClicks() {
+        return this.clicks;
+    }
+
+    public int getViews() {
+        return this.views;
+    }
+
+    public void incClicks() {
+        this.clicks++;
+    }
+
+    public void incViews() {
+        this.views++;
+    }
+
+    public void describeMe() {
+        System.out.println("Name: BaseAdvertising\n" +
+                "Description: This is base class for Ad and Advertiser classes.");
     }
 }
