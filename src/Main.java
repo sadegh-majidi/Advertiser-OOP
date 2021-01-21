3050596672b5b612c58635c38ea17447da38e79a
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-21 17:06:13 +0330

    Complete main method for running

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import adModels.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        BaseAdvertising baseAdvertising = new BaseAdvertising();
+        Advertiser advertiser1 = new Advertiser(1, "name1");
+        Advertiser advertiser2 = new Advertiser(2, "name2");
+        Ad ad1 = new Ad(1, "title1", "img-url1", "link1", advertiser1);
+        Ad ad2 = new Ad(2, "title2", "img-url2", "link2", advertiser2);
+
+        baseAdvertising.describeMe();
+        ad2.describeMe();
+        advertiser1.describeMe();
+
+        ad1.incViews();
+        ad1.incViews();
+        ad1.incViews();
+        ad1.incViews();
+        ad2.incViews();
+        ad1.incClicks();
+        ad1.incClicks();
+        ad2.incClicks();
+
+        System.out.println(advertiser2.getName());
+        advertiser2.setName("new name");
+        System.out.println(advertiser2.getName());
+
+        System.out.println(ad1.getClicks());
+        System.out.println(advertiser2.getClicks());
+        System.out.println(Advertiser.getTotalClicks());
+
+        Advertiser.help();
     }
 }
