7136c80b46b361100483341b7fa23423b40c3619
Sadegh Majidi <mohammadsadeghmajidiyazdi@hotmail.com>
2021-01-21 16:50:09 +0330

    Add methods to Ad class

--- src/adModels/Ad.java
+++ src/adModels/Ad.java
@@ -14,4 +14,50 @@ public class Ad extends BaseAdvertising {
         this.link = link;
         this.advertiser = advertiser;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String url) {
+        this.imgURL = url;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setAdvertiser(Advertiser advertiser) {
+        this.advertiser = advertiser;
+    }
+
+    @Override
+    public void incClicks() {
+        super.incClicks();
+        this.advertiser.incClicks();
+    }
+
+    @Override
+    public void incViews() {
+        super.incViews();
+        this.advertiser.incViews();
+    }
+
+    @Override
+    public void describeMe() {
+        System.out.println("Name: Ad\n" +
+                "Description: This class is a blueprint to create advertisement objects and derived from BaseAdvertising Base class.");
+    }
 }
